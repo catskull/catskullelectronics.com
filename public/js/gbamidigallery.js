5c475f88116fb2d86502a5f56748a06bb946cc82
David DeGraw <david.degraw@gmail.com>
2019-04-03 22:36:48 -0600

    Update gba midi with v2

--- public/js/gbamidigallery.js
+++ public/js/gbamidigallery.js
@@ -7,7 +7,7 @@ function cycle(direction) {
     subtitle = document.getElementById('gallery_subtitle');
 
     if (direction) {
-      if (i == 5){
+      if (i == 4){
         i = 1;
       }
       else {
@@ -16,7 +16,7 @@ function cycle(direction) {
     }
     else {
       if (i == 1){
-        i = 5;
+        i = 4;
       }
       else {
         i = i - 1;
@@ -25,33 +25,27 @@ function cycle(direction) {
 
     switch (i) {
       case 1:
-        image.src = baseurl + '1.png';
-        image.alt = "GBA MIDI Synth with link cable";
-        text.innerHTML = "1/5";
+        image.src = baseurl + '1.jpg';
+        image.alt = "GBA MIDI Synth with link cable and TRS adapter";
+        text.innerHTML = "1/4";
         break;
 
       case 2:
-        image.src = baseurl + '2.png';
-        image.alt = "DIN MIDI"
-        text.innerHTML = "2/5";
+        image.src = baseurl + '2.jpg';
+        image.alt = "Heat shrink for protection"
+        text.innerHTML = "2/4";
         break;
 
       case 3:
-        image.src = baseurl + '3.png';
-        image.alt = "GBA link port";
-        text.innerHTML = "3/5";
+        image.src = baseurl + '3.jpg';
+        image.alt = "MIDI TRS port";
+        text.innerHTML = "3/4";
         break;
 
       case 4:
-        image.src = baseurl + '4.png';;
-        image.alt = "Wrapped in clear heat shrink for protection"
-        text.innerHTML = "4/5";
-        break;
-
-      case 5:
-        image.src = baseurl + '5.png';;
-        image.alt = "GBA ROM included in the synth"
-        text.innerHTML = "5/5";
+        image.src = baseurl + '4.jpg';;
+        image.alt = "PCB link port connector"
+        text.innerHTML = "4/4";
         break;
     }
 
